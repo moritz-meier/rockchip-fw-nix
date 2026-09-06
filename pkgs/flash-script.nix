668c01ce0c8560d170537af3f8c3481692bc5517
Moritz Meier <git@mam2911.slmail.me>
2026-09-06 17:45:57 +0200

    env in emmc

--- pkgs/flash-script.nix
+++ pkgs/flash-script.nix
@@ -25,8 +25,6 @@ writeShellScript "${name}-flash-${target}.sh" ''
   ${rkdeveloptool}/bin/rkdeveloptool db ${loader}
 
   ${rkdeveloptool}/bin/rkdeveloptool cs ${toString targetIds.${target}}
-  ${rkdeveloptool}/bin/rkdeveloptool ef
-
   ${rkdeveloptool}/bin/rkdeveloptool wl ${toString offsets.${target}} ${bin}
 
   ${rkdeveloptool}/bin/rkdeveloptool rd
